0e1b0f31d695d9d2a32939c14ed48f5c41117d39
Andrew Burgess <andrew@andrewburgess.io>
2016-07-19 14:34:09 -0700

    :sparkles: feat: allow options to be specified per request parameter schema

--- src/index.js
+++ src/index.js
@@ -12,16 +12,23 @@ export default (schema = {}) => {
         const validate = promisify(Joi.validate);
         let validations = [];
 
+        let defaultOptions = {
+            abortEarly: false
+        };
+
         if (schema.query) {
-            validations.push(validate(_.omit(req.query, 'token'), schema.query));
+            let options = _.extend({}, defaultOptions, _.get(schema, 'options.query'));
+            validations.push(validate(_.omit(req.query, 'token'), schema.query, options));
         }
 
         if (schema.params) {
-            validations.push(validate(req.params, schema.params));
+            let options = _.extend({}, defaultOptions, _.get(schema, 'options.params'));
+            validations.push(validate(req.params, schema.params, options));
         }
 
         if (schema.body) {
-            validations.push(validate(req.body, schema.body));
+            let options = _.extend({}, defaultOptions, _.get(schema, 'options.body'));
+            validations.push(validate(req.body, schema.body, options));
         }
 
         return Promise.all(validations)
--- test/index.js
+++ test/index.js
@@ -81,6 +81,27 @@ describe('middleware/joi', () => {
                 expect(spy).to.have.been.calledWith({ num: 5 });
             });
         });
+
+        it('should allow options to be specified for query schema', () => {
+            let next = sinon.spy();
+
+            let middleware = joi({
+                query: { num: Joi.number().required() },
+                options: {
+                    query: {
+                        allowUnknown: true
+                    }
+                }
+            });
+
+            return Promise.resolve(
+                middleware({ query: { num: 5 } }, {}, next)
+            )
+            .then(() => {
+                expect(next).to.have.been.calledOnce.and.calledWithExactly();
+                expect(spy.firstCall.args[2]).to.deep.equal({ abortEarly: false, allowUnknown: true });
+            });
+        });
     });
 
     describe('path', () => {
@@ -110,6 +131,27 @@ describe('middleware/joi', () => {
                 expect(next).to.have.been.called;
                 expect(next.firstCall.args[0]).to.be.an.instanceof(Errors.BadRequestError);
                 expect(next.firstCall.args[0].fields).to.contain('num');
+            });
+        });
+
+        it('should allow options to be specified for params schema', () => {
+            let next = sinon.spy();
+
+            let middleware = joi({
+                params: { num: Joi.number().required() },
+                options: {
+                    params: {
+                        allowUnknown: true
+                    }
+                }
+            });
+
+            return Promise.resolve(
+                middleware({ params: { num: 5 } }, {}, next)
+            )
+            .then(() => {
+                expect(next).to.have.been.calledOnce.and.calledWithExactly();
+                expect(spy.firstCall.args[2]).to.deep.equal({ abortEarly: false, allowUnknown: true });
             });
         });
     });
@@ -143,6 +185,27 @@ describe('middleware/joi', () => {
                 expect(next.firstCall.args[0].fields).to.contain('num');
             });
         });
+
+        it('should allow options to be specified for body schema', () => {
+            let next = sinon.spy();
+
+            let middleware = joi({
+                body: { num: Joi.number().required() },
+                options: {
+                    body: {
+                        allowUnknown: true
+                    }
+                }
+            });
+
+            return Promise.resolve(
+                middleware({ body: { num: 5 } }, {}, next)
+            )
+            .then(() => {
+                expect(next).to.have.been.calledOnce.and.calledWithExactly();
+                expect(spy.firstCall.args[2]).to.deep.equal({ abortEarly: false, allowUnknown: true });
+            });
+        });
     });
 
     it('should pass to next handler if Joi errors', () => {
@@ -152,7 +215,7 @@ describe('middleware/joi', () => {
             json: sinon.stub().returnsThis()
         };
         spy.restore();
-        spy = sinon.stub(Joi, 'validate').callsArg(3, err);
+        spy = sinon.stub(Joi, 'validate').callsArg(4, err);
         let middleware = joi({ query: { test: Joi.number() } });
         let next = sinon.spy();
 
